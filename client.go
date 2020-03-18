bad7c4e132eef8daa2f3f69511a0902e3b8a217d
Tomaž Hribernik <tomaz.hribernik@protonmail.com>
2020-03-18 18:58:02 +0100

    Update package paths to reflect fork

--- client.go
+++ client.go
@@ -14,8 +14,8 @@ import (
 	"os"
 	"time"
 
-	"github.com/adshao/go-binance/common"
-	"github.com/adshao/go-binance/futures"
+	"github.com/Tomiyou/go-binance/common"
+	"github.com/Tomiyou/go-binance/futures"
 	"github.com/bitly/go-simplejson"
 )
 
